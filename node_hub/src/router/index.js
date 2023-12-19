f6bd9640e219f47d16ba7caf0633685c8e61099f
niannian <898885815@qq.com>
2023-12-19 14:26:57 +0800

    路由注册修改

--- node_hub/src/router/index.js
+++ node_hub/src/router/index.js
@@ -2,20 +2,31 @@ const userRouter = require('../view/user/router')
 const momentRouter = require('../view/moment/router')
 const commentRouter = require('../view/comment/router')
 const tagRouter = require('../view/tag/router')
+const fs = require('fs')
+const path = require('path')
+
+const routePath = path.join(__dirname, '../view')
+const filePaths = fs.readdirSync(routePath)
 
 
 const useRoutes = function() {
-  this.use(userRouter.routes());
-  this.use(userRouter.allowedMethods());
+  filePaths.forEach(file => {
+    if(file === '.DS_Store') return
+    const router = require(`../view/${file}/router`)
+    this.use(router.routes());
+    this.use(router.allowedMethods());
+  })
+  // this.use(userRouter.routes());
+  // this.use(userRouter.allowedMethods());
 
-  this.use(momentRouter.routes());
-  this.use(momentRouter.allowedMethods());
+  // this.use(momentRouter.routes());
+  // this.use(momentRouter.allowedMethods());
 
-  this.use(commentRouter.routes());
-  this.use(commentRouter.allowedMethods());
+  // this.use(commentRouter.routes());
+  // this.use(commentRouter.allowedMethods());
 
-  this.use(tagRouter.routes());
-  this.use(tagRouter.allowedMethods());
+  // this.use(tagRouter.routes());
+  // this.use(tagRouter.allowedMethods());
 }
 // const useRoutes = function() {
 //   fs.readdirSync(__dirname).forEach(file => {
